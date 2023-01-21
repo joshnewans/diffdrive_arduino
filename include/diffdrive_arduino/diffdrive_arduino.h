d0126dcbd5444a475a72c7768cebdc6deca851a0
Buzzology <chrisowens321@gmail.com>
2023-01-21 13:28:16 +0000

    add: includes

--- include/diffdrive_arduino/diffdrive_arduino.h
+++ include/diffdrive_arduino/diffdrive_arduino.h
@@ -4,36 +4,34 @@
 #include <cstring>
 #include "rclcpp/rclcpp.hpp"
 
-#include "hardware_interface/base_interface.hpp"
 #include "hardware_interface/system_interface.hpp"
 #include "hardware_interface/handle.hpp"
 #include "hardware_interface/hardware_info.hpp"
 #include "hardware_interface/types/hardware_interface_return_values.hpp"
-#include "hardware_interface/types/hardware_interface_status_values.hpp"
+#include "rclcpp_lifecycle/state.hpp"
 
 #include "config.h"
 #include "wheel.h"
 #include "arduino_comms.h"
 
-
 using hardware_interface::return_type;
 
-class DiffDriveArduino : public hardware_interface::BaseInterface<hardware_interface::SystemInterface>
+class DiffDriveArduino : public hardware_interface::SystemInterface
 {
 
 
 public:
   DiffDriveArduino();
 
-  return_type configure(const hardware_interface::HardwareInfo & info) override;
+  CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
 
   std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
 
   std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
 
-  return_type start() override;
+  CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
 
-  return_type stop() override;
+  CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
 
   return_type read() override;
 
